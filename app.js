e5f6a17f3b2290960c5b515b3aae366ce52a015c
Troy Gerwien <yortus@gmail.com>
2023-05-07 17:47:31 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,81 @@
-
-require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.8.3/min/vs' }});
-window.MonacoEnvironment = { getWorkerUrl: () => proxy };
-
-let proxy = URL.createObjectURL(new Blob([`
-	self.MonacoEnvironment = {
-		baseUrl: 'https://unpkg.com/monaco-editor@0.8.3/min/'
-	};
-	importScripts('https://unpkg.com/monaco-editor@0.8.3/min/vs/base/worker/workerMain.js');
-`], { type: 'text/javascript' }));
-
 let monacoEditor;
 
+require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.38.0/min/vs' }});
 require(["vs/editor/editor.main"], function () {
-	monacoEditor = monaco.editor.create(document.getElementById('pen'), {
-		value: [
-			`start = "abc"`,
-            ``
-		].join('\n'),
-		language: 'text',
-		theme: 'vs-dark',
+    monaco.languages.register({
+        id: 'pen'
+    });
+    monaco.languages.setMonarchTokensProvider('pen', {
+        defaultToken: 'invalid',
+
+        operators: [
+            '=', '|', ':', ',', '*', '?', '+', '!', '&',
+        ],
+
+        tokenizer: {
+            root: [
+                {include: '@comment'},
+                {include: '@keyword'},
+
+                {include: '@bindingPattern'},
+				{include: '@bindingName'},
+
+				{include: '@stringX'},
+				{include: '@stringA'},
+				{include: '@stringC'},
+				{include: '@identifier'},
+
+                [/[{}()\[\]]/, '@brackets'],
+                [/./, {
+                    cases: {
+                        '@operators': 'operator',
+                        '@default'  : '',
+                    }
+                }],
+            ],
+
+            comment: [
+                [/\/\/.*$/, 'comment'],
+            ],
+
+            keyword: [
+                // TODO
+                ['__parse|__print', 'keyword']
+            ],
+
+            bindingPattern: [
+                // TODO
+            ],
+            bindingName: [
+                // TODO
+            ],
+
+            stringX: [
+                [/"[^"\r\n]*"/, 'variable.other.pen'],
+            ],
+            stringA: [
+                [/'[^'\r\n]*'/, 'support.function.pen'],
+            ],
+            stringC: [
+                [/`[^`\r\n]*`/, 'string.quoted.pen'],
+            ],
+            identifier: [
+                [/[_A-Z][_A-Z0-9]*(?!\w)/, 'string.quoted.pen'],
+                [/\w+(?=\s*:)/, 'support.function.pen'],
+                [/\w+/, 'variable.other.pen'],
+            ],
+        },
+    });
+
+    monacoEditor = monaco.editor.create(document.getElementById('pen'), {
+        value: localStorage.getItem('pen') ?? `start = "abc"\n`,
+        language: 'pen',
+        theme: 'vs-dark',
         automaticLayout: true,
-	});
-	
-	// monacoEditor.addListener('didType', () => {
-	// 	console.log(monacoEditor.getValue());
-	// });
+    });
 });
+
+setInterval(() => localStorage.setItem('pen', monacoEditor.getValue()), 2000);
 
 function parse() {
     const console = document.getElementById('console');
